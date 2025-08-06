b47e6a386e413cc9512f0b8ad8c02928413c1a45
Subhashree-Nayak507 <kukisubhashree@gmail.com>
2025-08-06 14:07:37 +0530

    minor fix

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,6 +1,5 @@
 import { logoutController } from '@/controllers/authController';
-import { NextRequest } from 'next/server';
 
 export async function POST(request: Request) {
-    return await logoutController(request as NextRequest);
+    return await logoutController();
 }
